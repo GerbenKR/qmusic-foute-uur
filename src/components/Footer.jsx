8229b74c1306e60fc02420ea4564c499e36b0d76
GerbenKr <gerbiebor@outlook.com>
2022-04-15 11:05:15 +0200

    maked footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div className=" bg-q-carbon-gray text-white p-10 mt-12 flex items-center gap-10 justify-center">
+        <div className=" bg-q-carbon-gray text-white p-10 mt-12 flex items-center justify-center flex-wrap gap-10 ">
             <p className="text-[20px]">
                 Created with ❤️ by{' '}
                 <a className="text-q-red underline" target="_blank" href="https://gerbenkr.nl">
