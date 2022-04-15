e96cda3db3c878c1d60958fda05c9694fa3e22ba
GerbenKr <gerbiebor@outlook.com>
2022-04-15 11:05:52 +0200

    Added winner message

--- src/components/TrackHolder.jsx
+++ src/components/TrackHolder.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TrackHolder({ track }) {
     if (!track) return <p>Er gaat iets fout..</p>;
@@ -9,9 +9,17 @@ export default function TrackHolder({ track }) {
             <h1 className="text-4xl mt-2">{track.title}</h1>
             <h2 className="text-[20px]">{track.artist}</h2>
             <div className="mt-3 w-full h-10">
-                <div className="bg-q-carbon-gray h-full flex items-center justify-center" style={{ width: track.score.percentage + '%' }}>
-                    {Math.round(track.score.percentage)}% ({track.score.real})
-                </div>
+                {Math.round(track.score.percentage) != 100 && (
+                    <div className="bg-q-carbon-gray h-full flex items-center justify-center" style={{ width: track.score.percentage + '%' }}>
+                        {Math.round(track.score.percentage)}% ({track.score.real})
+                    </div>
+                )}
+
+                {Math.round(track.score.percentage) == 100 && (
+                    <div className="bg-green-500 h-full flex items-center justify-center" style={{ width: track.score.percentage + '%' }}>
+                        {Math.round(track.score.percentage) == 100 && <p>GEWONNEN! ({track.score.real})</p>}
+                    </div>
+                )}
             </div>
         </div>
     );
